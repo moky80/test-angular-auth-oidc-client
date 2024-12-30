42f91852be14ae8974a65fe0a0c24484bf6ad9a9
T V <tvdata2020@gmail.com>
2024-12-30 17:27:14 +0100

    Refine debug info, add authConfig google & facebook (experiment), add test log cognito (authorize: ok, silentRenew with refresh token: ok once but no more, refresh token was (errorneously?) removed by the lib)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OidcSecurityService, LoginResponse } from 'angular-auth-oidc-client';
-import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
+const DEFAULT_UNAUTHENTICATED_RESPONSE: any = {
+  configId: null,
+  isAuthenticated: false,
+  userData: null,
+};
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule],
@@ -15,23 +19,20 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   title = 'a19';
   private readonly oidcSecurityService = inject(OidcSecurityService);
-  private bAuthenticated = false;
-  userData$ = this.oidcSecurityService.userData$;
+  authenticatedResponse = DEFAULT_UNAUTHENTICATED_RESPONSE;
   ngOnInit() {
     this.checkAuth();
   }
   checkAuth() {
     this.oidcSecurityService
-      .checkAuth()
-      .subscribe((loginResponse: LoginResponse) => {
-        const { isAuthenticated, userData, accessToken, idToken, configId } =
-          loginResponse;
-        console.log('loginResponse:', loginResponse);
-        this.bAuthenticated = isAuthenticated
-      });
+    .checkAuthMultiple()
+    .subscribe((loginResponses: LoginResponse[]) => {
+      console.log('---loginResponses', loginResponses);
+      this.authenticatedResponse = loginResponses.find((loginResponse) => loginResponse?.isAuthenticated===true) || DEFAULT_UNAUTHENTICATED_RESPONSE;
+    });
   }
-  login() {
-    this.oidcSecurityService.authorize();
+  login(configIg: string) {
+    this.oidcSecurityService.authorize(configIg);
   }
   logout() {
     // The following is from AWS quick setup guide for Angular and worked.
@@ -52,15 +53,29 @@ export class AppComponent implements OnInit {
       .logoff()
       .subscribe((result) => {
         console.log('logoutResponse:', result);
+        if (this.authenticatedResponse.configId=== 'google') {
+          // logoff does not work for Google:
+          // https://github.com/damienbod/angular-auth-oidc-client/issues/1744
+          // google identity provider does not support logout, can't fix this on the client.
+          // Clear the session storage and reload the page to have the logout effect.
+          if (window.sessionStorage) {
+            window.sessionStorage.clear();
+          }
+          window.location.reload();
+        }
       });
   }
-  isAuthenticated(){
-    return this.bAuthenticated;
+  isAuthenticated(): Observable<boolean> {
+    return this.oidcSecurityService.isAuthenticated();
+  }
+  userData(): Observable<any> {
+    return this.oidcSecurityService.getUserData();
+  }
+  accessToken(): Observable<any>{
+    return this.oidcSecurityService.getAccessToken();
   }
   decodedAccessToken(): Observable<any>{
-    return this.oidcSecurityService.getAccessToken().pipe(
-      map(token => token ? jwtDecode(token) : "")
-    );
+    return this.oidcSecurityService.getPayloadFromAccessToken();
   }
   refreshToken(): Observable<any>{
     return this.oidcSecurityService.getRefreshToken();
--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,20 +1,59 @@
 import { PassedInitialConfig, LogLevel } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
-  config: {
-    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_74F2Q9zgh',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
-    clientId: '7e7si6l8sa9tdvoe7plrhe506',
-    scope: 'phone openid email profile', // 'openid profile offline_access ' + your scopes
-    responseType: 'code', // Authorization Code Flow
-    silentRenew: true,
-    useRefreshToken: true,
-    renewTimeBeforeTokenExpiresInSeconds: 30,
-    logLevel: LogLevel.Debug, // Enable debug logs
-    customParamsEndSessionRequest: {
-      client_id: '7e7si6l8sa9tdvoe7plrhe506',
-      logout_uri: window.location.origin,
+  config: [
+    {
+      configId: 'cognito',
+      authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_74F2Q9zgh',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: '7e7si6l8sa9tdvoe7plrhe506',
+      scope: 'phone openid email profile',
+      responseType: 'code', // Authorization Code Flow with Proof Key for Code Exchange (PKCE)
+      silentRenew: true,
+      useRefreshToken: true,
+      renewTimeBeforeTokenExpiresInSeconds: 30,
+      logLevel: LogLevel.Debug, // Enable debug logs
+      customParamsEndSessionRequest: {
+        client_id: '7e7si6l8sa9tdvoe7plrhe506',
+        logout_uri: window.location.origin,
+      }
+    },
+    {
+      configId: 'google',
+      authority: 'https://accounts.google.com',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: "206397179491-f4qcp8q8qk07hbhod81os0i01afbmab7.apps.googleusercontent.com",
+      scope: 'openid email profile',
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      renewTimeBeforeTokenExpiresInSeconds: 30,
+      logLevel: LogLevel.Debug,
+      autoUserInfo: true, // Fetch user information automatically
+      customParamsCodeRequest: {
+        /* client_secret: "dummy", TODO - NOT acceptable in production!!! */
+      }
+    },
+    {
+      // https://github.com/damienbod/angular-auth-oidc-client/issues/1826#issuecomment-2563089224
+      configId: 'facebook',
+      authority: 'https://facebook.com',
+      // override the default well-known endpoint to workaround CORS error from the
+      // default OIDC discovery endpoint https://facebook.com/.well-known/openid-configuration
+      authWellknownEndpointUrl: `${window.location.origin}/assets`,
+      authWellknownUrlSuffix: '', // override the default well-known url suffix
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: "3849494948641276",
+      scope: 'public_profile email openid',
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      renewTimeBeforeTokenExpiresInSeconds: 30,
+      logLevel: LogLevel.Debug,
+      autoUserInfo: true,
     }
-  }
+  ]
 }
